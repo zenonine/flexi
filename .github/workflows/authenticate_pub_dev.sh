8736511097b9fba7c3295912d9ef441067895caf
Nguyen <nguyenxndaidev@gmail.com>
2021-10-19 03:54:30 +0200

    0.1.1 (#4)
    Add workflow to publish package

---
+++ .github/workflows/authenticate_pub_dev.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# This script creates/updates credentials.json file which is used
+# to authorize publisher when publishing packages to pub.dev
+
+# Checking whether the secrets are available as environment
+# variables or not.
+if [ -z "${PUB_CACHE}" ]; then
+  echo "Missing PUB_CACHE environment variable"
+  exit 1
+fi
+
+if [ -z "${PUB_DEV_PUBLISH_ACCESS_TOKEN}" ]; then
+  echo "Missing PUB_DEV_PUBLISH_ACCESS_TOKEN environment variable"
+  exit 1
+fi
+
+if [ -z "${PUB_DEV_PUBLISH_REFRESH_TOKEN}" ]; then
+  echo "Missing PUB_DEV_PUBLISH_REFRESH_TOKEN environment variable"
+  exit 1
+fi
+
+if [ -z "${PUB_DEV_PUBLISH_ID_TOKEN}" ]; then
+  echo "Missing PUB_DEV_PUBLISH_ID_TOKEN environment variable"
+  exit 1
+fi
+
+if [ -z "${PUB_DEV_PUBLISH_EXPIRATION}" ]; then
+  echo "Missing PUB_DEV_PUBLISH_EXPIRATION environment variable"
+  exit 1
+fi
+
+# Create credentials.json file.
+cat <<EOF > "${PUB_CACHE}/credentials.json"
+{
+  "accessToken": "${PUB_DEV_PUBLISH_ACCESS_TOKEN}",
+  "refreshToken": "${PUB_DEV_PUBLISH_REFRESH_TOKEN}",
+  "idToken": "${PUB_DEV_PUBLISH_ID_TOKEN}",
+  "tokenEndpoint": "https://accounts.google.com/o/oauth2/token",
+  "scopes": [
+    "openid",
+    "https://www.googleapis.com/auth/userinfo.email"
+  ],
+  "expiration": ${PUB_DEV_PUBLISH_EXPIRATION}
+}
+EOF
